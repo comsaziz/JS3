6600558269c879b7ab2c130977214718d731431e
comsaziz <comsaziz2@gmail.com>
2023-11-17 19:39:00 +0300

    init

---
+++ main.js
@@ -0,0 +1,30 @@
+/*const myString = 'i love JS3'
+const regex  = /JS3/
+const containRegex = regex.test(myString)
+console.log(containRegex); */
+
+
+/*const myString = 'i love JS3'
+const regex  = /JS3/
+let newString = myString.replace(regex, 'Kotlin')
+console.log(newString); */
+
+
+/*const myString = 'i love JS3'
+let newString = myString.search(/JS3/)
+console.log(newString); */
+
+
+
+/*let ages = [25,28.29]
+let = [Firstages,secondage,thirdage] = ages
+console.log(Firstages,secondage,thirdage) */
+
+
+/*let student = {
+name : 'aziz'
+, age : 23
+
+}
+let { name , nickname = 'Unknown'} = student
+console.log(name,nickname) */
